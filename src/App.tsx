c67cbc75cdaa1f79645dd3b7cfca2f871a3e416f
Deepa Srivastav <deeps.srivastav368@gmail.com>
2023-08-12 18:19:58 +0530

    display genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,22 @@ function App() {
         base: `"nav" "main"`, //for mobile devices
         lg: `"nav nav" "aside main"`, //for screens larger than 1024px
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
         {/*items inside this will be rendered for devices with screen sizes lg and above. */}
-        <GridItem area="aside" bg="black">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
 
-      <GridItem area="main" bg="black">
+      <GridItem area="main">
         <GameGrid />
       </GridItem>
     </Grid>
